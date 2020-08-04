e6c93c141c511fdb301cabb362f65f4d8e120314
drlindsaybennett <lindsay.bennett@ncas.ac.uk>
2020-08-04 10:57:41 +0100

    Files for Z calibration - processing the uncalibrated netcdf, using notebooks to plot the results and determine the bias. Also a notebook for plotting the initial phase values determined from the processing.

---
+++ raine/process_raine_dbz.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PYTHONDIR=/gws/smf/j04/ncas_radar/software/anaconda3/envs/Clustering_2_7_15/
+zdr_dir=/gws/nopw/j04/ncas_radar_vol2/data/xband/raine/calibrations/ZDRcalib/
+logdir=/home/users/lbennett/logs/raine/cals
+mkdir -p $logdir/$TODAY/
+
+for day in $(find $zdr_dir  -maxdepth 1 -name "20*" -type d -execdir basename {} ';')
+#for day in 20200518 20200519 
+#for day in 20190722 
+do
+echo $day
+
+#bsub -o $logdir/$TODAY/$day'_norm.log' -e $logdir/$TODAY/$day'_norm.err' -q short-serial -R "rusage[mem=5000]" -W 02:00 $PYTHONDIR/bin/python -W ignore process_raine_dbz.py $day
+bsub -o $logdir/$TODAY/$day'_att.log' -e $logdir/$TODAY/$day'_att.err' -q short-serial -R "rusage[mem=5000]" -W 02:00 $PYTHONDIR/bin/python -W ignore process_raine_dbz.py $day
+done
+
