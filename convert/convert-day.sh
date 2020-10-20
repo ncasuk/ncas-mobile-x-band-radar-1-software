f1eb7bb0c219bf9413f7cc70093436bb13831db6
drlindsaybennett <lindsay.bennett@ncas.ac.uk>
2020-10-20 14:01:53 +0100

    added working folders back to cloud

---
+++ convert/convert-day.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Load defaults
+source raine_defaults.cfg
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+DATE=
+SCAN_TYPE=
+
+# Parse command-line arguments
+while getopts "t:d:" OPTION; do
+    case $OPTION in
+    t)
+        SCAN_TYPE=$OPTARG
+        ;;
+    d)
+        DATE=$OPTARG
+        ;;
+    *)
+        echo "[ERROR] Incorrect options provided"
+        exit 1
+        ;;
+    esac
+done
+
+
+if [[ ! $DATE ]] || [[ ${#DATE} -ne 8 ]] && [[ ! $DATE =~ /\d{8}/ ]]; then
+    echo "[ERROR] Date must be 8 digits, not: '$DATE'"
+    exit 1
+fi
+
+# Check validity of date 
+if [[ $DATE -lt $START_DATE ]] || [[ $DATE -gt $END_DATE ]]; then
+    echo "[ERROR] Date is out of range: $DATE"
+    echo "        Allowed range: $START_DATE - $END_DATE"
+    exit 1
+fi
+
+# Check scan type
+if [[ ! $SCAN_TYPE =~ ^(vol|ele|azi)$ ]]; then
+    echo "[ERROR] Scan type must be one of 'vol', 'ele', 'azi'."
+    exit 1
+fi
+
+n_chunks=$(expr 24 / $CHUNK_SIZE)
+
+if [[ $n_chunks -lt 1 ]]; then
+    echo "[ERROR] Please check CHUNK_SIZE in 'defaults.cfg' - must be 24 or less."
+    exit 1
+fi
+
+YYYY=$(date +%Y)
+MM=$(date +%m)
+DD=$(date +%d)
+
+output_basedir=$LOTUS_OUTPUTS_BASEDIR/raine/$YYYY/$MM/$DD
+mkdir -p $output_basedir
+
+chunk=0
+
+while [[ $chunk -lt 24 ]]; do
+
+    ARGS="-t $SCAN_TYPE"
+
+    for hour in $(seq $chunk $(expr $chunk + $CHUNK_SIZE - 1)); do
+
+        DATE_HOUR=${DATE}$(printf %02d ${hour})
+        ARGS="$ARGS $DATE_HOUR"
+
+    done
+
+    let chunk+=$CHUNK_SIZE
+     
+    echo "[INFO] Running for: $DATE_HOUR"
+
+    wallclock=$(printf %02d $((${CHUNK_SIZE} / 2))):00:00 
+    
+    output_base=$output_basedir/$(echo $ARGS | cut -d' ' -f3)
+
+    
+    print_cmd="sbatch -p $QUEUE -t $wallclock -o ${output_base}.out -e ${output_base}.err --wrap=\"time $SCRIPT_DIR/convert-raine-x-band-hour.sh ${ARGS}\""
+    echo "[INFO] Running: $print_cmd"
+
+    script_cmd=$(sbatch -p $QUEUE -t $wallclock -o ${output_base}.out -e ${output_base}.err --wrap="time $SCRIPT_DIR/convert-raine-x-band-hour.sh ${ARGS}")
+    echo $script_cmd
+
+done
+exit 0;
+
---
+++ convert/lib/defaults.py
@@ -0,0 +1,35 @@
+"""Default values that will be taken if no others are provided"""
+
+
+models = ['BCC/bcc-csm1-1', 'BCC/bcc-csm1-1-m', 'BNU/BNU-ESM', 'CCCma/CanAM4', 'CCCma/CanCM4',
+          'CCCma/CanESM2', 'CMCC/CMCC-CESM', 'CMCC/CMCC-CM', 'CMCC/CMCC-CMS',
+          'CNRM-CERFACS/CNRM-CM5', 'CNRM-CERFACS/CNRM-CM5-2', 'COLA-CFS/CFSv2-2011',
+          'CSIRO-BOM/ACCESS1-0', 'CSIRO-BOM/ACCESS1-3', 'CSIRO-QCCCE/CSIRO-Mk3-6-0',
+          'FIO/FIO-ESM', 'ICHEC/EC-EARTH', 'INM/inmcm4', 'IPSL/IPSL-CM5A-LR', 'IPSL/IPSL-CM5A-MR',
+          'IPSL/IPSL-CM5B-LR', 'LASG-CESS/FGOALS-g2', 'LASG-IAP/FGOALS-gl', 'LASG-IAP/FGOALS-s2',
+          'MIROC/MIROC-ESM', 'MIROC/MIROC-ESM-CHEM', 'MIROC/MIROC4h', 'MIROC/MIROC5', 'MOHC/HadCM3',
+          'MOHC/HadGEM2-A', 'MOHC/HadGEM2-CC', 'MOHC/HadGEM2-ES', 'MPI-M/MPI-ESM-LR',
+          'MPI-M/MPI-ESM-MR', 'MPI-M/MPI-ESM-P', 'MRI/MRI-AGCM3-2H', 'MRI/MRI-AGCM3-2S',
+          'MRI/MRI-CGCM3', 'MRI/MRI-ESM1', 'NASA-GISS/GISS-E2-H', 'NASA-GISS/GISS-E2-H-CC',
+          'NASA-GISS/GISS-E2-R', 'NASA-GISS/GISS-E2-R-CC', 'NASA-GMAO/GEOS-5', 'NCAR/CCSM4',
+          'NCC/NorESM1-M', 'NCC/NorESM1-ME', 'NICAM/NICAM-09', 'NIMR-KMA/HadGEM2-AO',
+          'NOAA-GFDL/GFDL-CM2p1', 'NOAA-GFDL/GFDL-CM3', 'NOAA-GFDL/GFDL-ESM2G',
+          'NOAA-GFDL/GFDL-ESM2M', 'NOAA-GFDL/GFDL-HIRAM-C180', 'NOAA-GFDL/GFDL-HIRAM-C360',
+          'NOAA-NCEP/CFSv2-2011', 'NSF-DOE-NCAR/CESM1-BGC', 'NSF-DOE-NCAR/CESM1-CAM5',
+          'NSF-DOE-NCAR/CESM1-CAM5-1-FV2', 'NSF-DOE-NCAR/CESM1-FASTCHEM',
+          'NSF-DOE-NCAR/CESM1-WACCM']
+
+ensembles = ['r10i1p1', 'r11i1p1', 'r12i1p1', 'r1i1p1', 'r1i1p121', 'r1i1p122', 'r1i1p124',
+             'r1i1p125', 'r1i1p126', 'r1i1p127', 'r1i1p128', 'r1i1p2', 'r1i1p3', 'r1i2p1',
+             'r1i2p2', 'r2i1p1', 'r2i1p2', 'r2i1p3', 'r3i1p1', 'r3i1p2', 'r3i1p3', 'r4i1p1',
+             'r4i1p2', 'r4i1p3', 'r5i1p1', 'r5i1p2', 'r5i1p3', 'r6i1p1', 'r6i1p2', 'r6i1p3',
+             'r7i1p1', 'r8i1p1', 'r9i1p1']
+
+variables = ['baresoilFrac', 'burntArea', 'c3PftFrac', 'c4PftFrac', 'cCwd', 'cLeaf', 'cLitter',
+             'cLitterAbove', 'cLitterBelow', 'cMisc', 'cProduct', 'cRoot', 'cSoil', 'cSoilFast',
+             'cSoilMedium', 'cSoilSlow', 'cVeg', 'cWood', 'cropFrac', 'evspsblsoi', 'evspsblveg',
+             'fFire', 'fGrazing', 'fHarvest', 'fLitterSoil', 'fLuc', 'fVegLitter', 'fVegSoil',
+             'gpp', 'grassFrac', 'lai', 'landCoverFrac', 'mrfso', 'mrlsl', 'mrro', 'mrros', 'mrso',
+             'mrsos', 'nbp', 'nep', 'npp', 'nppLeaf', 'nppRoot', 'nppWood', 'pastureFrac', 'prveg',
+             'rGrowth', 'rMaint', 'ra', 'residualFrac', 'rh', 'shrubFrac', 'tran', 'treeFrac',
+             'treeFracPrimDec', 'treeFracPrimEver', 'treeFracSecDec', 'treeFracSecEver', 'tsl']
