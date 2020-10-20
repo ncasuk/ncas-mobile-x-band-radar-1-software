dd20f57586998701c72fa4a37b6d5ac40c279ffb
drlindsaybennett <lindsay.bennett@ncas.ac.uk>
2020-10-20 13:40:13 +0100

    added calc_calib, convert and apply_cacl to gitignore file

--- calc_calib/process_vert_scans_main.py
+++ calc_calib/process_vert_scans_main.py
@@ -6,7 +6,9 @@ from datetime import date
 today = date.today().strftime("%Y-%m-%d")
 
 if not os.path.exists(os.path.join(SETTINGS.LOG_DIR,today)):
-    os.mkdir(os.path.join(SETTINGS.LOG_DIR,today))
+    os.makedirs(os.path.join(SETTINGS.LOG_DIR,today))
+if not os.path.exists(os.path.join(SETTINGS.LOTUS_DIR,today)):
+    os.makedirs(os.path.join(SETTINGS.LOTUS_DIR,today))
 
 dates = [os.path.basename(x) for x in glob.glob(SETTINGS.DATA_DIR+'20*')]
 
@@ -15,10 +17,10 @@ current_directory = os.getcwd()  # get current working directory
 for day in dates[0:1]:
     print(day)
 
-# submit to lotus
+# command to submit to lotus
     sbatch_command = f"sbatch -p {SETTINGS.QUEUE} -t {SETTINGS.WALLCLOCK} -o " \
                        f"{SETTINGS.LOTUS_DIR}{today}/{day}.out -e {SETTINGS.LOTUS_DIR}{today}/{day}.err "\
-                       f"--wrap=\"{current_directory} python process_raine_vert_scans.py {day} {SETTINGS.PLOT}\""
+                       f"--wrap=\"python {current_directory}/process_raine_vert_scans.py {day} {SETTINGS.PLOT}\""
 
     #subprocess.call(sbatch_command, shell=True)
 
