70a0e3176beeaa59328442744e2a3a0686a642ee
drlindsaybennett <lindsay.bennett@ncas.ac.uk>
2020-10-21 15:25:36 +0100

    added new directory structure

---
+++ calc_calib/process_vert_scans_main.py
@@ -0,0 +1,28 @@
+import SETTINGS
+import os
+import glob
+from datetime import date
+
+today = date.today().strftime("%Y-%m-%d")
+
+if not os.path.exists(os.path.join(SETTINGS.LOG_DIR,today)):
+    os.makedirs(os.path.join(SETTINGS.LOG_DIR,today))
+if not os.path.exists(os.path.join(SETTINGS.LOTUS_DIR,today)):
+    os.makedirs(os.path.join(SETTINGS.LOTUS_DIR,today))
+
+dates = [os.path.basename(x) for x in glob.glob(SETTINGS.DATA_DIR+'20*')]
+
+current_directory = os.getcwd()  # get current working directory
+
+for day in dates[0:1]:
+    print(day)
+
+# command to submit to lotus
+    sbatch_command = f"sbatch -p {SETTINGS.QUEUE} -t {SETTINGS.WALLCLOCK} -o " \
+                       f"{SETTINGS.LOTUS_DIR}{today}/{day}.out -e {SETTINGS.LOTUS_DIR}{today}/{day}.err "\
+                       f"--wrap=\"python {current_directory}/process_raine_vert_scans.py {day} {SETTINGS.PLOT}\""
+
+    #subprocess.call(sbatch_command, shell=True)
+
+    print(f"running {sbatch_command}")
+
---
+++ calc_calib/setup_env.sh
@@ -0,0 +1,1 @@
+PYTHONDIR=/gws/smf/j04/ncas_radar/software/anaconda3/envs/Clustering_2_7_15
