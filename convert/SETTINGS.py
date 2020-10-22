60aa5b11f8b3f9b97c507376f7d996c7cd822389
Jonathan Haigh <jhaigh0@sci2.jasmin.ac.uk>
2020-10-22 09:50:00 +0100

    Set up base files and settings for project

---
+++ convert/SETTINGS.py
@@ -0,0 +1,28 @@
+# Project Choice
+PROJ_NAME = 'raine' # raine or chilbolton
+
+EXIT_AFTER_N_FAILURES = 1000000
+MIN_START_DATE = '20181025'
+MAX_END_DATE = '20201230'
+
+# lotus settings
+QUEUE = 'short-serial'
+CHUNK_SIZE = 6 # in hours
+
+LOTUS_OUTPUT = f'/home/users/lbennett/logs/lotus-output/{PROJ_NAME}'
+
+# Radex convert params file
+PARAMS_FILE=f'/home/users/lbennett/lrose/ingest_params/{PROJ_NAME}/RadxConvert.{PROJ_NAME}.uncalib'
+
+# choice for success / failure output handling
+BACKEND = 'db' #'db' or 'file'
+
+# input dirs
+CHIL_DIR = '/gws/nopw/j04/ncas_obs/amf/raw_data/ncas-mobile-x-band-radar-1/data/chilbolton'
+RAINE_DIR = '/gws/nopw/j04/ncas_obs/amf/raw_data/ncas-mobile-x-band-radar-1/incoming/raine'
+
+INPUT_DIR = '/gws/nopw/j04/ncas_obs/amf/raw_data/ncas-mobile-x-band-radar-1/incoming/raine'
+
+# output dirs (for netcdf)
+OUTPUT_DIR=f'/gws/nopw/j04/ncas_radar_vol2/data/xband/{PROJ_NAME}/cfradial/uncalib_v1'
+
---
+++ convert/convert_time_series.py
@@ -0,0 +1,5 @@
+"""
+Converts raw data (vol, ele, or azi) from given radar (raine or chilbolton) 
+over a time range into netcdf files. """
+
+import SETTINGS
