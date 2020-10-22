2ccf0dd708da38a7cc153b7b447f121041cc371a
drlindsaybennett <lindsay.bennett@ncas.ac.uk>
2020-10-22 10:09:20 +0100

    edited python path

--- calc_calib/setup_env.sh
+++ calc_calib/setup_env.sh
@@ -1,1 +1,2 @@
-PYTHONDIR=/gws/smf/j04/ncas_radar/software/anaconda3/envs/Clustering_2_7_15
+#PYTHONDIR=/gws/smf/j04/ncas_radar/software/anaconda3/envs/Clustering_2_7_15
+PYTHONDIR=/gws/smf/j04/ncas_radar/software/miniconda3_radar_group_20200519/envs/pyart_3_8_radar_group/bin/python
