f0dacb41fc431cc1125ff371b85fbfe38c0bdbf0
drlindsaybennett <lindsay.bennett@ncas.ac.uk>
2020-10-29 15:29:03 +0000

    added comparison of args with permissible date range

--- calc_calib/process_vert_scans_time_series.py
+++ calc_calib/process_vert_scans_time_series.py
@@ -1,9 +1,8 @@
 import SETTINGS
 import os
-import glob
-from datetime import date
 import argparse
 import dateutil.parser as dp
+import datetime import date
 
 def arg_parse_all():
     """
@@ -33,21 +32,37 @@ def loop_over_days()
     """
 
     today = date.today().strftime("%Y-%m-%d")
-    
+    #Set up directory for Lotus output files based on today's date
     if not os.path.exists(os.path.join(SETTINGS.LOTUS_DIR,today)):
         os.makedirs(os.path.join(SETTINGS.LOTUS_DIR,today))
-    
-    dates = os.listdir(SETTINGS.DATA_DIR)
-    dates.sort()
+
+    start_date = args.start_date
+    end_date = args.end_date
+
+    start_date_dt = dp.parse(start_date) 
+    end_date_dt = dp.parse(end_date) 
+  
+    min_date = dp.parse(SETTINGS.MIN_START_DATE)
+    max_date = dp.parse(SETTINGS.MAX_END_DATE)
+ 
+    if start_date_dt < min_date or end_date_dt > max_date:
+        raise ValueError(f'Date must be in range {SETTINGS.MIN_START_DATE} - {SETTINGS.MAX_END_DATE}')
+ 
+    proc_dates = os.listdir(SETTINGS.DATA_DIR)
+    proc_dates.sort()
     
     current_directory = os.getcwd()  # get current working directory
-    for day in dates:
+
+    for day in proc_dates:
+        day_dt=dp.parse(day);
+        if day_dt >= start_date_dt and day_dt <= end_date_dt:
+    
         print(day)
     
     # command to submit to lotus
         sbatch_command = f"sbatch -p {SETTINGS.QUEUE} -t {SETTINGS.WALLCLOCK} -o " \
                            f"{SETTINGS.LOTUS_DIR}{today}/{day}.out -e {SETTINGS.LOTUS_DIR}{today}/{day}.err "\
-                           f"--wrap=\"python {current_directory}/process_vert_scans.py {day} {SETTINGS.PLOT}\""
+                           f"--wrap=\"python {current_directory}/process_vert_scans_day.py {day} {SETTINGS.PLOT}\""
     
         #subprocess.call(sbatch_command, shell=True)
     
